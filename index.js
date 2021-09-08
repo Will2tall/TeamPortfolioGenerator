ed5c271ea7f1caa42bcd81c8ad74cb08274147af
Terror666 <ckuthulu@gmail.com>
2021-09-08 00:32:59 -0500

    new inquirer prompts

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -9,184 +12,17 @@ const promptUser = () => {
             choices: ['Manager', 'Engineer', 'Intern', 'Finish']    
         }
     ])
+    .then(({ employeeList }) => {
+        if(employeeList === 'Manager') {
+            new Manager;
+        } else if(employeeList === 'Engineer') {
+            new Engineer;
+        } else if(employeeList === 'Intern') {
+            new Intern;
+        } else {
+            console.log(Manager)
+        }
+    })
 }
 
-const managerQuestion = manData => {
-    if (!manData.info) {
-        manData.info = [];
-    }
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'mName',
-            message: "Please enter your manager's name (required)",
-            validate: mNameInput => {
-               if (mNameInput) {
-                   return true;
-               } else {
-                   console.log('Please enter a name')
-                   return false;
-               }
-            }
-        },
-        {
-            type: 'number',
-            name: 'mId',
-            message: "Please enter manager's ID number (required)",
-            validate: mIdInput => {
-                if (mIdInput === Number) {
-                    return true;
-                } else {
-                    console.log('Please enter a number for ID number')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'mEmail',
-            message: "Please enter manager's email address (required)",
-            validate: mEmailInput => {
-                if(mEmailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter an Email address')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'number',
-            name: 'office',
-            message: "Please enter manager's office number (required)",
-            validate: officeInput => {
-                if (officeInput === Number) {
-                    return true;
-                } else {
-                    console.log('Please enter a number for office')
-                    return false;
-                }
-            }
-        }
-    ])
-}
-
-const engineerQuestion = engData => {
-    if (!engData.info) {
-        engData.info = [];
-    }
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'eName',
-            message: "Please enter your engineer's name (required)",
-            validate: eNameInput => {
-               if (eNameInput) {
-                   return true;
-               } else {
-                   console.log('Please enter a name')
-                   return false;
-               }
-            }
-        },
-        {
-            type: 'number',
-            name: 'eId',
-            message: "Please enter engineer's ID number (required)",
-            validate: eIdInput => {
-                if (eIdInput === Number) {
-                    return true;
-                } else {
-                    console.log('Please enter a number for ID number')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'eEmail',
-            message: "Please enter engineer's email address (required)",
-            validate: eEmailInput => {
-                if(eEmailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter an Email address')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: "Please enter engineer's github username (required)",
-            validate: eGithubInput => {
-                if(eGithubInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a github username')
-                    return false;
-                }
-            }
-        }
-    ])
-}
-
-const InternQuestion = intData => {
-    if (!intData.info) {
-        intData.info = [];
-    }
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'iName',
-            message: "Please enter your intern's name (required)",
-            validate: iNameInput => {
-               if (iNameInput) {
-                   return true;
-               } else {
-                   console.log('Please enter a name')
-                   return false;
-               }
-            }
-        },
-        {
-            type: 'number',
-            name: 'iId',
-            message: "Please enter intern's ID number (required)",
-            validate: iIdInput => {
-                if (iIdInput === Number) {
-                    return true;
-                } else {
-                    console.log('Please enter a number for ID number')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'iEmail',
-            message: "Please enter intern's email address (required)",
-            validate: iEmailInput => {
-                if(iEmailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter an Email address')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: "Please enter what school your intern attended",
-            validate: schoolInput => {
-                if(schoolInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a school')
-                    return false;
-                }
-            }
-        }
-    ])
-}
+promptUser();
